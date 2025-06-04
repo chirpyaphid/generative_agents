6c448154918534f37e28d15eb4a210e36f25f16d
chirpyaphid <peterlouw1973@gmail.com>
2025-06-04 09:53:43 +0100

    Add landing page test

--- environment/frontend_server/translator/tests.py
+++ environment/frontend_server/translator/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TranslatorViewTests(TestCase):
+    """Tests for views in the translator app."""
+
+    def test_landing_page(self):
+        """Landing view should return HTTP 200."""
+        response = self.client.get(reverse("landing"))
+        self.assertEqual(response.status_code, 200)
+
